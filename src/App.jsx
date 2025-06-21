27974bf874cbcb233a05c9672b40689f8e74751a
DevendraK29 <devendrakamble2907@gmail.com>
2025-06-21 17:37:20 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import AboutPage from "./pages/AboutPage"
 import CoursesPage from "./pages/CoursesPage"
 import ContactPage from "./pages/ContactPage"
 import AdmissionPage from "./pages/AdmissionPage"
+import ChatbotComponent from "./components/Chatbot/ChatbotComponents";
+// import Footer from "./components/Footer/Footer";
+// import Header from "./components/Header/Header";
 const App = () => {
     return(
         <>
@@ -16,6 +19,7 @@ const App = () => {
               <Route path="/contact" element={<ContactPage/>}/>
               <Route path="/admission" element={<AdmissionPage/>}/>
             </Routes>
+            <ChatbotComponent />
           </Router>              
         </>
     )
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header/Header";
+// import '../styles/AboutPage.css'
 
 const AboutPage = () => {
     return(
